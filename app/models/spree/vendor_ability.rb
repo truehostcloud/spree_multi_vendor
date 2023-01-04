1e185ea6a9c594503c96384c1324d47bddcda4a5
Collins Lagat <cklagat@gmail.com>
2023-01-04 13:28:18 +0300

    feat: allow vendors to update master pricing for each currency

--- app/models/spree/vendor_ability.rb
+++ app/models/spree/vendor_ability.rb
@@ -69,7 +69,7 @@ class Spree::VendorAbility
   end
 
   def apply_price_permissions
-    can :modify, Spree::Price, variant: { vendor_id: @vendor_ids }
+    can [:admin, :manage], Spree::Price, variant: { vendor_id: @vendor_ids }
   end
 
   def apply_product_option_type_permissions
